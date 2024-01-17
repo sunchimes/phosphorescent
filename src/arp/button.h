0aca6de136fb0ae3cf69fc67a9fdcf038d792efa
Riley Wong <rileynwong@gmail.com>
2024-01-16 21:54:23 -0500

    Fix header errors and add button.cpp to Makefile targets

--- src/arp/button.h
+++ src/arp/button.h
@@ -1,25 +1,29 @@
-// button.h 
 #include <string>
+
 #include "daisy_seed.h"
 #include "daisysp.h"
 
-using namespace daisy;
+#ifndef BUTTON_H // include guard
+#define BUTTON_H
 
-#ifndef BUTTON // include guard
-#define BUTTON
-
-namespace button 
+class Button
 {
-    class Button
-    {
-        public:
-            Switch hwButton;
-            std::string pitchName; // Pitch name, e.g. A3
-            float frequency; // e.g. 440
-            bool isHeld;
-            Button(Switch hwButton, std::string pitchName, float frequency, bool isHeld);
-    };
-    static std::vector<button::Button> buttons;
-}
+    private:
+        daisy::Switch hwButton;
+        std::string pitchName{}; // Pitch name, e.g. A3
+        float frequency{}; // e.g. 440
+        bool isHeld{};
+
+    public:
+        Button(daisy::Switch hwButton, std::string pitchName, float frequency, bool isHeld);
+
+        daisy::Switch getHwButton() const { return hwButton; }
+        std::string getPitchName() const { return pitchName; }
+        float getFrequency() const { return frequency; }
+        bool getIsHeld() const { return isHeld; }
+
+};
+
+// static std::vector<Button::Button> buttons;
 
 #endif
